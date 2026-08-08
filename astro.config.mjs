b3489a5588a36f0c710a01c4d57c752f7b0d6b29
Dany <na-klasse09@yandex.ru>
2026-08-08 14:37:36 +0300

    Исправить основной адрес сайта для SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://npd-tools.ru',
+  site: 'https://samozanyatye.vercel.app',
   output: 'static',
   trailingSlash: 'never',
   integrations: [
